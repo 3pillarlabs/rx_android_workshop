7671770253e00fe84998cd5dd8532afc10aa89e8
Ciprian Grigor <ciprian.grigor@3pillarglobal.com>
2016-02-13 21:18:26 +0200

    rxjava and first unit test

--- app/src/test/java/com/tpg/movierx/ExampleUnitTest.java
+++ app/src/test/java/com/tpg/movierx/ExampleUnitTest.java
@@ -2,7 +2,10 @@ package com.tpg.movierx;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import rx.Observable;
+import rx.Subscriber;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -11,5 +14,36 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testObservable() {
+        final StringBuilder result = new StringBuilder();
+
+        Observable.just(1, 2, 3).subscribe(new Subscriber<Integer>() {
+            @Override
+            public void onCompleted() {
+                msg("onCompleted");
+                result.append('|');
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                msg("onError " + e);
+                result.append('X');
+            }
+
+            @Override
+            public void onNext(Integer integer) {
+                msg("onNext " + integer);
+                result.append(integer);
+            }
+        });
+
+        assertEquals("subcriber callbacks", "123|", result.toString());
+    }
+
+    private void msg(String message) {
+        System.out.println(message);
     }
 }
