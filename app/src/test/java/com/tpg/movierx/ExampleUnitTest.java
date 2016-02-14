c90a06ecea1592da1bed250f7bf02b40c73964a3
Ciprian Grigor <ciprian.grigor@3pillarglobal.com>
2016-02-14 15:00:00 +0200

    test Observable.interval with TestSubscriber

--- app/src/test/java/com/tpg/movierx/ExampleUnitTest.java
+++ app/src/test/java/com/tpg/movierx/ExampleUnitTest.java
@@ -2,8 +2,12 @@ package com.tpg.movierx;
 
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Subscriber;
+import rx.functions.Func1;
+import rx.observers.TestSubscriber;
 
 import static org.junit.Assert.assertEquals;
 
@@ -41,6 +45,29 @@ public class ExampleUnitTest {
         });
 
         assertEquals("subcriber callbacks", "123|", result.toString());
+    }
+
+    @Test
+    public void testIntervalObservable() {
+        TestSubscriber<Long> testSubscriber = new TestSubscriber<>();
+
+        Observable.interval(500, TimeUnit.MILLISECONDS)
+                .filter(new Func1<Long, Boolean>() {
+                    @Override
+                    public Boolean call(Long number) {
+                        return number % 2 == 0;
+                    }
+                })
+                .take(5)
+                .subscribe(testSubscriber);
+
+        testSubscriber.awaitTerminalEvent();
+
+        testSubscriber.assertCompleted();
+        testSubscriber.assertUnsubscribed();
+        testSubscriber.assertNoErrors();
+        testSubscriber.assertValueCount(5);
+        testSubscriber.assertValues(0L, 2L, 4L, 6L, 8L);
     }
 
     private void msg(String message) {
