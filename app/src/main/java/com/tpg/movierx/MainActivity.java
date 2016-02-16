81e85b24f16cb42edfef7504b2e1d0793660dc6a
Ciprian Grigor <ciprian.grigor@3pillarglobal.com>
2016-02-16 18:06:43 +0200

    Handle error; set executor and receiver thread

--- app/src/main/java/com/tpg/movierx/MainActivity.java
+++ app/src/main/java/com/tpg/movierx/MainActivity.java
@@ -1,6 +1,7 @@
 package com.tpg.movierx;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -18,6 +19,8 @@ import java.util.Arrays;
 import javax.inject.Inject;
 
 import butterknife.Bind;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends BaseActivity {
 
@@ -43,7 +46,6 @@ public class MainActivity extends BaseActivity {
         adapter = new MoviePopupAdapter();
         popup.setAdapter(adapter);
 
-        adapter.setMovieList(Arrays.asList(new OmdbMovie("Title 1"), new OmdbMovie("Title 2")));
 
         searchText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -54,9 +56,15 @@ public class MainActivity extends BaseActivity {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 if (!TextUtils.isEmpty(s)) {
-                    omdbApi.searchByTitle(s.toString()).subscribe(omdbSearchMovies -> {
+                    omdbApi.searchByTitle(s.toString())
+                            .subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(omdbSearchMovies -> {
                         adapter.setMovieList(omdbSearchMovies.movies);
                         popup.show();
+                            }, throwable -> {
+                                logger.error("searching error", throwable);
+                                Snackbar.make(searchText, throwable.toString(), Snackbar.LENGTH_LONG).show();
                     });
                 } else {
                     popup.dismiss();
