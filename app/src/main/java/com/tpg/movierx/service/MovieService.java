f17b88abfe252a275412afef9b73583457557260
Ciprian Grigor <ciprian.grigor@3pillarglobal.com>
2016-02-16 21:53:24 +0200

    Cancel obsolete search requests; avoid too many request; transform error; retry

--- app/src/main/java/com/tpg/movierx/service/MovieService.java
+++ app/src/main/java/com/tpg/movierx/service/MovieService.java
@@ -3,6 +3,8 @@ package com.tpg.movierx.service;
 import com.tpg.movierx.omdb.OmdbApi;
 import com.tpg.movierx.omdb.OmdbSearchMovies;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -23,12 +25,16 @@ public class MovieService {
     }
 
     public Observable<OmdbSearchMovies> searchMovie(Observable<CharSequence> inputObservable) {
-        return inputObservable.map(CharSequence::toString)
-                .flatMap(this::searchMovie);
+        return inputObservable
+                .debounce(250, TimeUnit.MILLISECONDS)//avoid too many requests
+                .map(CharSequence::toString)
+                .switchMap(this::searchMovie);
     }
 
     public Observable<OmdbSearchMovies> searchMovie(String title) {
         return omdbApi.searchByTitle(title)
+                .retry(2)
+                .onErrorReturn(OmdbSearchMovies::new) //never fail
                 .subscribeOn(Schedulers.io());
     }
 
